fcc9cd85041304807c308d92911f93573fdb62f7
AngelJV128a <angelj.vazquez1@gmail.com>
2025-06-07 22:27:32 -0600

    grid con todos los autores y ya paginados funcionando

--- app/authors/page.jsx
+++ app/authors/page.jsx
@@ -1,17 +1,77 @@
+"use client";
 import CardAuthor from "@/components/authors/CardAuthor";
 import { Card } from "@/components/ui/card";
+import { useState, useEffect } from "react";
+import Cookies from "js-cookie";
+import Swal from "sweetalert2";
+import axios from "axios";
+import ReactPaginate from "react-paginate";
+import { Button } from "@/components/ui/button";
 
 export default function PageAuthors() {
+  const [authors, setAuthors] = useState([]);
+  const [currentPage, setCurrentPage] = useState(0);
+  const [totalPages, setTotalPages] = useState(0);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const token = Cookies.get("token");
+      console.log(`token ${token}`);
+      const page = currentPage + 1;
+      try {
+        const response = await axios.get(
+          `${process.env.NEXT_PUBLIC_API_URL}/authors`,
+          {
+            params: { page },
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        const json = response.data;
+        console.log(json);
+        setTotalPages(json.last_page);
+        setAuthors(json.data);
+      } catch (error) {
+        console.log(error);
+        Swal.fire({
+          title: "Error",
+          text: "No se pudo cargar los autores",
+          icon: "error",
+          confirmButtonText: "Aceptar",
+        });
+      }
+    };
+    fetchData();
+  }, [currentPage]);
+
+  const handlePageClick = ({ selected }) => {
+    console.log("selected", selected);
+    setCurrentPage(selected);
+  };
   return (
     <>
       <div className="flex flex-col items-center px-4">
         <h1 className="text-2xl font-bold mb-4">Authors</h1>
 
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 w-8/9">
-          <CardAuthor />
-          <CardAuthor />
-          <CardAuthor />
+          {authors.map((author) => (
+            <CardAuthor key={author.id} author={author} />
+          ))}
         </div>
+        <ReactPaginate
+          breakLabel="..."
+          nextLabel={<Button variant="outline">Siguiente</Button>}
+          previousLabel={<Button variant="outline">Anterior</Button>}
+          pageCount={totalPages}
+          onPageChange={handlePageClick}
+          containerClassName="flex justify-center items-center gap-2 mt-6 flex-wrap"
+          pageClassName="px-1"
+          activeClassName="bg-primary text-primary-foreground rounded"
+          pageLinkClassName="cursor-pointer px-3 py-1 rounded hover:bg-primary hover:text-primary-foreground mt-2 mb-2"
+          disabledClassName="opacity-50 cursor-not-allowed"
+          renderOnZeroPageCount={null}
+        />
       </div>
     </>
   );
--- components/authors/CardAuthor.jsx
+++ components/authors/CardAuthor.jsx
@@ -9,16 +9,16 @@ import {
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 
-export default function CardAuthor({ index }) {
+export default function CardAuthor({ author }) {
   return (
     <Card
-      key={index}
+      key={author.id}
       className="max-w-3xl  items-start gap-6 p-4 transform transition-transform duration-300 hover:scale-105"
     >
       <div className="flex items-start">
         <div className="flex relative w-24 h-24 rounded-full overflow-hidden flex-shrink-0">
           <img
-            src="https://randomuser.me/api/portraits/men/32.jpg"
+            src="https://www.gravatar.com/avatar/?d=mp"
             alt="Foto del autor"
             className="object-cover"
             sizes="96px"
@@ -26,14 +26,14 @@ export default function CardAuthor({ index }) {
         </div>
 
         <CardContent className="flex flex-col ml-6 p-0">
-          <CardTitle className="text-xl">Juan Pérez</CardTitle>
+          <CardTitle className="text-xl">{author.name + " " + author.last_name}</CardTitle>
           <CardDescription className="text-sm text-muted-foreground">
-            Nacionalidad: Mexicana
+            {author.nationality}
           </CardDescription>
         </CardContent>
       </div>
       <CardFooter className="ml-auto p-0">
-        <Button className="cursor-pointer">Ver perfil</Button>
+        <Button className="cursor-pointer">Ver Libros </Button>
       </CardFooter>
     </Card>
   );
