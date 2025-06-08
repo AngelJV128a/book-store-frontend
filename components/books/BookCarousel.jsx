13197b6965aba4d6368c35edc8a3a9fd6e41a832
AngelJV128a <angelj.vazquez1@gmail.com>
2025-06-07 22:42:45 -0600

    grid y carousel de libros destacados ahora es dinamico y se alimenta de un endpoint que devuelve 10 libros random

--- components/books/BookCarousel.jsx
+++ components/books/BookCarousel.jsx
@@ -4,8 +4,11 @@ import { Card, CardContent } from "@/components/ui/card";
 import { useKeenSlider } from "keen-slider/react";
 import "keen-slider/keen-slider.min.css";
 import { useEffect, useState } from "react";
+import Cookies from "js-cookie";
+import Swal from "sweetalert2";
+import axios from "axios";
 
-const libros = [
+/* const libros = [
   { titulo: "Libro 1", imagen: "https://picsum.photos/200?random=1" },
   { titulo: "Libro 2", imagen: "https://picsum.photos/200?random=2" },
   { titulo: "Libro 3", imagen: "https://picsum.photos/200?random=3" },
@@ -16,9 +19,11 @@ const libros = [
   { titulo: "Libro 8", imagen: "https://picsum.photos/200?random=8" },
   { titulo: "Libro 9", imagen: "https://picsum.photos/200?random=9" },
   { titulo: "Libro 10", imagen: "https://picsum.photos/200?random=10" },
-];
+]; */
 
 export default function BookCarousel() {
+  const [libros, setLibros] = useState([]);
+
   const [sliderRef, instanceRef] = useKeenSlider({
     loop: true,
     slides: {
@@ -32,11 +37,41 @@ export default function BookCarousel() {
     },
   });
 
-  // Auto-slide effect
+  useEffect(() => {
+    const fetchData = async () => {
+      const token = Cookies.get("token");
+      console.log(`token ${token}`);
+      try {
+        const response = await axios.get(
+          `${process.env.NEXT_PUBLIC_API_URL}/books/random`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        const json = response.data;
+        console.log(json);
+        setLibros(json.data);
+      } catch (error) {
+        console.log(error);
+        Swal.fire({
+          title: "Error",
+          text: "No se pudo cargar los libros",
+          icon: "error",
+          confirmButtonText: "Aceptar",
+        });
+      }
+    };
+    fetchData();
+  }, []);
+
   useEffect(() => {
     const interval = setInterval(() => {
-      instanceRef.current?.next();
-    }, 5000); // 10 seconds
+      if (instanceRef.current) {
+        instanceRef.current.next();
+      }
+    }, 5000);
 
     return () => clearInterval(interval);
   }, [instanceRef]);
@@ -49,11 +84,11 @@ export default function BookCarousel() {
           <Card key={idx} className="keen-slider__slide shadow-md">
             <CardContent className="p-2 flex flex-col items-center">
               <img
-                src={libro.imagen}
-                alt={libro.titulo}
+                src={libro.image}
+                alt={libro.title}
                 className="w-full h-48 object-cover rounded-lg"
               />
-              <p className="mt-2 font-semibold text-center">{libro.titulo}</p>
+              <p className="mt-2 font-semibold text-center">{libro.title}</p>
             </CardContent>
           </Card>
         ))}
--- components/books/GridBook.jsx
+++ components/books/GridBook.jsx
@@ -1,16 +1,53 @@
+"use client";
 import { Book, Grid } from "lucide-react";
 import Image from "next/image";
 import CardBook from "./CardBook";
+import { useState, useEffect } from "react";
+import Cookies from "js-cookie";
+import Swal from "sweetalert2";
+import axios from "axios";
 
 export default function GridBook() {
+
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const token = Cookies.get("token");
+      console.log(`token ${token}`);
+      try {
+        const response = await axios.get(
+          `${process.env.NEXT_PUBLIC_API_URL}/books/random`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        const json = response.data;
+        console.log(json);
+        setBooks(json.data);
+      } catch (error) {
+        console.log(error);
+        Swal.fire({
+          title: "Error",
+          text: "No se pudo cargar los libros",
+          icon: "error",
+          confirmButtonText: "Aceptar",
+        });
+      }
+    };
+    fetchData();
+  },[]);
+
   return (
     <>
       <div className="px-4 py-8">
         <h2 className="text-2xl font-bold mb-4 text-center">Libros destacados</h2>
 
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 justify-items-center">
-          {[...Array(8)].map((_, index) => (
-            <CardBook key={index} index={index} />
+          {books.map((book) => (
+            <CardBook key={book.id} book={book} />
           ))}
         </div>
       </div>
