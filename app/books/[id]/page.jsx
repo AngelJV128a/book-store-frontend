d2e953f680ee0de8d86b5b4324b6a8a35bd1b04c
AngelJV128a <angelj.vazquez1@gmail.com>
2025-06-12 16:54:03 -0600

    ya aparecen libros de la misma categoria como relacionados en la page de detalles de un libro

--- app/books/[id]/page.jsx
+++ app/books/[id]/page.jsx
@@ -1,9 +1,9 @@
 "use client";
 import BookDetail from "@/components/books/BookDetails";
+import CardBook from "@/components/books/CardBook";
 import { BookCardSkeleton } from "@/components/ui/bookCardSkeleton";
 import axios from "axios";
 import Cookies from "js-cookie";
-import { Book } from "lucide-react";
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
@@ -11,6 +11,7 @@ export default function DetailsBook() {
   const params = useParams();
 
   const [book, setBook] = useState({});
+  const [books, setBooks] = useState([]);
 
   useEffect(() => {
     const fetchBook = async () => {
@@ -30,6 +31,8 @@ export default function DetailsBook() {
         const json = response.data;
         console.log(json);
         setBook(json.book);
+        setBooks(json.related_books.data);
+        console.log(json.related_books.data);
       } catch (error) {
         console.log(error);
       }
@@ -48,6 +51,15 @@ export default function DetailsBook() {
 
         <div className=" gap-6 justify-items-center">
           <BookDetail book={book} />
+        </div>
+
+        <div className="mt-6 items-center">
+          <h1 className="text-2xl font-bold mb-4">Libros relacionados</h1>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 justify-items-center">
+            {books.map((book) => (
+              <CardBook key={book.id} book={book} />
+            ))}
+          </div>
         </div>
       </div>
     </>
